343c7e20ea431b330e4f8ad57e7e417e21b2743e
0xtdvt <davidzapata5885@gmail.com>
2024-01-13 09:23:55 -0500

    testnet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Web3 from 'web3';
 import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
@@ -6,162 +7,210 @@ import './styles/App.css';
 import migrateContractAbi from './contracts/migrateABI'
 import crtPolygonContractAbi from './contracts/erc20ABI';
 import crtAvalancheContractAbi from './contracts/erc20ABI';
+import { useAccount, useContractRead, usePrepareContractWrite, useContractWrite } from 'wagmi';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function App() {
 
+  const { openConnectModal } = useConnectModal();
+
+  const { address, isConnecting, isConnected, isDisconnected } = useAccount();
+
   // Estados para almacenar el proveedor, cuenta, red y balance de CRT
-  const [provider, setProvider] = useState(null);
+  const [walletAddress, setWalletAddress] = useState('');
+  const [showWalletField, setShowWalletField] = useState(false);
+
+  const [selectedToken, setSelectedToken] = useState('CRT'); // 'CRT' o 'BXT'
+  const [bxtPolygonBalance, setBxtPolygonBalance] = useState('0');
   const [account, setAccount] = useState(null);
-  const [network, setNetwork] = useState(null);
-  const [networkName, setNetworkName] = useState('');
   const [crtPolygonBalance, setCrtPolygonBalance] = useState('0');
   const [isMigrateDisabled, setIsMigrateDisabled] = useState(true);
   const [migrationMessage, setMigrationMessage] = useState('');
-
-  // Escuchar cambios en la cadena y cuentas de MetaMask
-  useEffect(() => {
-    if (provider) {
-      provider.on('chainChanged', handleNetworkChange);
-    }
-    return () => {
-      if (provider) {
-        provider.off('chainChanged', handleNetworkChange);
-      }
-    }
-  }, [provider]);
+  const [currentChainId, setCurrentChainId] = useState(null);
 
   useEffect(() => {
-    if (provider) {
-      provider.on('accountsChanged', handleAccountsChanged);
-    }
-  
-    return () => {
-      if (provider) {
-        provider.off('accountsChanged', handleAccountsChanged);
+    const fetchChainId = async () => {
+      if (typeof window !== 'undefined' && window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          const networkId = await web3.eth.net.getId();
+          setCurrentChainId(networkId);
+        } catch (e) {
+          console.error('Error fetching chainId:', e);
+        }
       }
     };
-  }, [provider]);
+    fetchChainId();
+
+    if (isConnected) {
+      switchToPolygonNetwork();
+    }
+
+    console.log("Conectado:", isConnected);
+    console.log("Chain ID:", currentChainId);
+    console.log("Address:", address);
+  }, [isConnected, currentChainId]);
+
+  useEffect(() => {
+    const web3 = new Web3(window.ethereum);
+  
+    // Función para actualizar el Chain ID
+    const updateChainId = async () => {
+      const networkId = await web3.eth.net.getId();
+      setCurrentChainId(networkId);
+    };
+  
+    // Listener para cambios en la red
+    window.ethereum.on('chainChanged', updateChainId);
+  
+    // Eliminar el listener cuando el componente se desmonte
+    return () => {
+      window.ethereum.removeListener('chainChanged', updateChainId);
+    };
+  }, []);
+
+  const switchToPolygonNetwork = async () => {
+    try {
+      await window?.ethereum?.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: "0x13881"}], // 0x13881  es el chainId para Polygon
+      });
+    } catch (switchError) {
+      console.error(switchError);
+    }
+  };
+
+  
   
   // Cargar balance de CRT cuando la cuenta o la red cambien
   useEffect(() => {
-    if (account && network === '80001') {
-      loadCrtBalance();
+    if (isConnected && currentChainId === 80001n) {
+      loadTokenBalance();
     }
-  }, [account, network]);
+  }, [address, isConnected, currentChainId, selectedToken]);
 
   // Habilitar o deshabilitar el botón de migración basado en el balance de CRT
   useEffect(() => {
-    setIsMigrateDisabled(crtPolygonBalance <= '1');
-  }, [crtPolygonBalance]);
+    if (selectedToken === 'CRT') {
+      setIsMigrateDisabled(crtPolygonBalance <= '1');
+    } else if (selectedToken === 'BXT') {
+      setIsMigrateDisabled(bxtPolygonBalance <= '1');
+    }
+  }, [crtPolygonBalance, bxtPolygonBalance, selectedToken]);
+
+  const handleWalletAddressChange = (event) => {
+    setWalletAddress(event.target.value);
+    console.log("Estado:", showWalletField);
+  };
+
+  const loadTokenBalance = async () => {
+    const web3 = new Web3(window.ethereum);
   
-  // Función para cargar el balance de CRT del usuario
-  const loadCrtBalance = async () => {
-    const web3 = new Web3(provider); // Inicializa web3 con el proveedor de Metamask
-    const crtContractAddress = '0xc8C06a58E4ad7c01b9bb5Af6C76a7a1CfEBd0319'; // Reemplaza con la dirección de tu contrato
-    const crtContract = new web3.eth.Contract(crtPolygonContractAbi, crtContractAddress);
-
+    let contractAddress;
+    let contractAbi;
+  
+    if (selectedToken === 'CRT') {
+      contractAddress = '0xc8C06a58E4ad7c01b9bb5Af6C76a7a1CfEBd0319'; // Dirección de CRT
+      contractAbi = crtPolygonContractAbi;
+    } else if (selectedToken === 'BXT') {
+      contractAddress = '0x133f734BD21198a60B6f5B6e5Ed08dDf3e357112'; // Dirección de BXT
+      contractAbi = crtPolygonContractAbi; // ABI de BXT
+    }
+  
+    const tokenContract = new web3.eth.Contract(contractAbi, contractAddress);
+  
     try {
-      const balance = await crtContract.methods.balanceOf(account).call();
-      setCrtPolygonBalance(web3.utils.fromWei(balance, 'ether')); // Convierte de Wei a Ether si es necesario
+      const balance = await tokenContract.methods.balanceOf(address).call();
+      if (selectedToken === 'CRT') {
+        setCrtPolygonBalance(web3.utils.fromWei(balance, 'ether'));
+      } else if (selectedToken === 'BXT') {
+        setBxtPolygonBalance(web3.utils.fromWei(balance, 'ether'));
+      }
     } catch (error) {
-      console.error('Error al cargar el balance de CRT:', error);
+      console.error('Error al cargar el balance:', error);
     }
   };
 
-  // Manejar cambios en la red de blockchain
-  const handleNetworkChange = async (_chainId) => {
-    const network = await provider.request({ method: 'net_version' });
-    setNetwork(network);
-    const networkName = getNetworkName(network);
-    setNetworkName(networkName);
-  }
-
-  // Manejar cambios en las cuentas de MetaMask
-  const handleAccountsChanged = (accounts) => {
-    if (accounts.length === 0) {
-      console.log("Por favor, conecta a MetaMask.");
-    } else {
-      setAccount(accounts[0]);
-      loadCrtBalance();
-    }
-  };
 
   // Función para migrar tokens CRT
   const migrateTokens = async () => {
-    if (!account || network !== '80001') {
+    if (!isConnected || currentChainId !== 80001n) {
       return;
     }
 
-
+    let contractAbi, contractAddress, migrateContractAddress;
+    if (selectedToken === 'CRT') {
+      contractAbi = crtAvalancheContractAbi;
+      contractAddress = '0xc8C06a58E4ad7c01b9bb5Af6C76a7a1CfEBd0319'; // Dirección de CRT
+      migrateContractAddress = '0xDe04de0FA2eC8057E0f0b5a4ABe3a75e5fF4bB98';
+    } else if (selectedToken === 'BXT') {
+      contractAbi = crtPolygonContractAbi; // ABI de BXT
+      contractAddress = '0x133f734BD21198a60B6f5B6e5Ed08dDf3e357112'; // Dirección de BXT
+      migrateContractAddress = '0xAE3fc22D0302d95Eb75a8C84e5C81510eb83C276';
+    }
   
-    const web3 = new Web3(provider);
-    const crtContractAddress = '0xc8C06a58E4ad7c01b9bb5Af6C76a7a1CfEBd0319'; // Polygon CRT address 
-    const crtContract = new web3.eth.Contract(crtAvalancheContractAbi, crtContractAddress);
+    const web3 = new Web3(window.ethereum);
+    const tokenContract = new web3.eth.Contract(contractAbi, contractAddress);
 
-    const migrateContractAddress = '0x25846e920BCb9DfaA9bd297a5Ec2500cdF763F32';
+    
     const migrateContract = new web3.eth.Contract(migrateContractAbi, migrateContractAddress);
   
     try {
       // transfer
-      const balanceWei = await crtContract.methods.balanceOf(account).call();
+      const balanceWei = await tokenContract.methods.balanceOf(address).call();
 
-      const currentAllowance = await crtContract.methods.allowance(account, migrateContractAddress).call();
+      const currentAllowance = await tokenContract.methods.allowance(address, migrateContractAddress).call();
 
       if (currentAllowance < balanceWei) {
-        const approveTx = await crtContract.methods.approve(migrateContractAddress, balanceWei).send({ from: account });
+        const approveTx = await tokenContract.methods.approve(migrateContractAddress, balanceWei).send({ from: address });
         console.log("Approve Tx:", approveTx);
       }
 
-      const migrateTx = await migrateContract.methods.migrate().send({ from: account });
-      console.log("Migrate Tx:", migrateTx);
+      if (showWalletField && walletAddress) {
+        // Validar dirección de la wallet
+        if (!web3.utils.isAddress(walletAddress)) {
+          setMigrationMessage("La wallet que ingresaste no es correcta");
+          return;
+        }
+        setMigrationMessage("");
+        // Llamada a la nueva función de migración con la dirección de la wallet
+        const migrateTx = await migrateContract.methods.migrateCustomWallet(walletAddress).send({ from: address });
+        console.log("Migrate Tx to Address:", migrateTx);
+      } else {
+        // Llamada a la función de migración original
+        const migrateTx = await migrateContract.methods.migrate().send({ from: address });
+        console.log("Migrate Tx:", migrateTx);
+      }
 
-      setMigrationMessage("Migración exitosa! Agrega el token: 0xb583D7191C41D9dbE86a08dae80D64926BF4C9bE");
-      await loadCrtBalance();
+      setTimeout(() => {
+        loadTokenBalance();
+      }, 3000);
+
+      // Mensaje de éxito personalizado dependiendo del token seleccionado
+      if (selectedToken === 'CRT') {
+        setMigrationMessage("Migración exitosa! Agrega el token: 0xcRt");
+      } else if (selectedToken === 'BXT') {
+        setMigrationMessage("Migración exitosa! Agrega el token: 0xbXt");
+      }
+      
   
     } catch (error) {
       console.error('Error al migrar tokens:', error);
       setMigrationMessage("Hubo un fallo en la migración, intenta nuevamente.");
     }
   };
-  
-  // Obtener el nombre de la red basado en el identificador de la red
-  const getNetworkName = (network) => {
-    switch(network) {
-      case '80001':
-        return 'Mumbai';
-      case '43114':
-        return 'Avalanche';
-      case '43113':
-        return 'Fuji';
-      default:
-        return 'Otra';
-    }
-  }
 
-  // Conectar con la wallet de MetaMask
-  async function connectMetamask() {
-    const provider = await detectEthereumProvider();
-    if (provider) {
-      setProvider(provider);
-      const accounts = await provider.request({ method: 'eth_requestAccounts' });
-      setAccount(accounts[0]);
-      const network = await provider.request({ method: 'net_version' });
-      setNetwork(network);
-      const networkName = getNetworkName(network);
-      setNetworkName(networkName);
-      if (!['80001', '43114', '43113'].includes(network)) {
-      }
-    }
-  }
 
   return (
-    <div className='bgd'>
+      <div className='bgd'>
       <Navbar className="navbar-custom justify-content-between">
-        <Navbar.Brand href="#home" className="text-light ml-3">CRT V2</Navbar.Brand>
+        <Navbar.Brand href="#home" className="text-light ml-3">MIGRACIÓN</Navbar.Brand>
         <Nav className="mr-auto"></Nav>
         <Form inline className="ml-auto">
           {account && (
-              <span className="text-light button-container">Red: {networkName}</span>
+              <span className="text-light button-container"></span>
           )}
           {account ? (
             <div className="button-container">
@@ -169,22 +218,48 @@ function App() {
             </div>
           ) : (
             <div className="button-container">
-              <Button variant="outline-info" className="metamask-button" onClick={connectMetamask}>Conectar a Metamask</Button>
+              <ConnectButton />
             </div>
           )}
         </Form>
       </Navbar>
-      {account && (
+      {isConnected && (
         <div className="parent-container">
-          {['80001'].includes(network) ? (
+          {currentChainId === 80001n ? (
             <div className="container">
+              <div className="token-selector">
+                <button className="token-button" onClick={() => setSelectedToken('CRT')}>CRT</button>
+                <button className="token-button" onClick={() => setSelectedToken('BXT')}>BXT</button>
+              </div>
               <h1 className="title">¡AVALANCHE TE ESPERA!</h1>
+        
               <div className="migrate-box">
                   <p className="total-migrate">Total a migrar</p>
-                  <p>{crtPolygonBalance} CRT</p>
+                  <p>{selectedToken === 'CRT' ? crtPolygonBalance : bxtPolygonBalance} {selectedToken}</p>
+              </div>
+              <div className="wallet-field">
+                <div>
+                  <button onClick={() => setShowWalletField(!showWalletField)}>
+                    {showWalletField ? 'Ocultar Wallet' : 'Especificar Wallet'}
+                  </button>
+                </div>
+                
+                {showWalletField && (
+                  <div className="migrate-box">
+                    <input
+                      className='input-box'
+                      type="text"
+                      placeholder="0x..."
+                      value={walletAddress}
+                      onChange={handleWalletAddressChange}
+                    />
+                  </div>
+                )}
+                
+                
               </div>
               <button className="migrate-button" onClick={migrateTokens} disabled={isMigrateDisabled}>
-                {isMigrateDisabled ? 'No tienes CRT para migrar' : 'MIGRAR'}
+                {isMigrateDisabled ? `No tienes ${selectedToken} para migrar` : `MIGRAR ${selectedToken}`}
               </button>
               <div>
                 {migrationMessage && <p>{migrationMessage}</p>}
@@ -196,7 +271,11 @@ function App() {
         </div>
       )}
     </div>
+    
   );
 }
 
 export default App;
+
+
+//----------------------------------//
--- src/index.js
+++ src/index.js
@@ -4,10 +4,70 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    connectorsForWallets,
+    getDefaultWallets,
+    RainbowKitProvider,
+  } from '@rainbow-me/rainbowkit';
+import {
+    argentWallet,
+    trustWallet,
+    ledgerWallet,
+    coreWallet,
+  } from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+    polygon,
+    polygonMumbai,
+  } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const projectId = process.env.REACT_APP_PROJECT_ID;
+
+  const {chains, publicClient, webSocketPublicClient} = configureChains(
+    [polygon, polygonMumbai],
+    [publicProvider()]
+  );
+
+  const {wallets} = getDefaultWallets({
+    appName: "Migrate",
+    projectId,
+    chains,
+  });
+
+  const demoAppInfo = {
+    appName: "Migrate",
+  };
+
+  const connectors = connectorsForWallets([
+    ...wallets,
+  ]);
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+    webSocketPublicClient,
+  });
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+
+root.render(
+    <React.StrictMode>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </React.StrictMode>
+  );
+  
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  serviceWorker.unregister();
